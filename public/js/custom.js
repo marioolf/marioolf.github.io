f83e7a7a6d94bacee415d9c2e5b478d71ab0d9bb
Mario López <mario.lopez@ikerlan.es>
2026-09-17 16:08:23 +0200

    Limit new-tab behavior to post links

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,12 +3,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
   anchors.forEach(function (anchor) {
     var href = anchor.getAttribute('href');
+    var url;
 
     if (!href || href.charAt(0) === '#') {
       return;
     }
 
     if (href.indexOf('mailto:') === 0 || href.indexOf('tel:') === 0 || href.indexOf('javascript:') === 0) {
+      return;
+    }
+
+    url = new URL(href, window.location.origin);
+
+    if (url.origin !== window.location.origin) {
+      anchor.removeAttribute('target');
+      anchor.removeAttribute('rel');
+      return;
+    }
+
+    if (!url.pathname.startsWith('/posts/') || url.pathname === '/posts/') {
+      anchor.removeAttribute('target');
+      anchor.removeAttribute('rel');
       return;
     }
 
